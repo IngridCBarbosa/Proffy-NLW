0cc7bffad780d1f524d740f840f5293e2b5b2ef5
IngridCBarbosa <ingridcarolinelima@hotmail.com>
2020-08-05 14:32:08 -0300

    add rote to list classes

--- server/README.md
+++ server/README.md
@@ -8,5 +8,5 @@
 ## Classes
 
     - [X] Rote to create one classes;
-    - [] Rote tp list all classes;
+    - [] Rote to list all classes;
         - [] Filter by subject, day on week and time;
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,60 +1,14 @@
 import express from 'express'
-import db from './database/connection';
-import convertHourToMinutes from './utils/convertHourToMinutes';
+import ClassesController from './controllers/ClassesController';
+
 
 const routes = express.Router();
 
-interface ScheduleItem {
-    week_day: number,
-    from: string,
-    to: string
-}
+const classesController = new ClassesController();
 
-routes.post('/classes', async (request, response) => {
-    const { name, avatar, whatsapp, bio, subject, cost, schedule } = request.body;
+routes.post('/classes',  classesController.create);
 
-    const trx = await db.transaction();
-
-    try {
-        const insertedUsersId = await trx('users').insert({
-            name,
-            avatar,
-            whatsapp,
-            bio
-        });
-
-        const user_id = insertedUsersId[0];
-
-        const insertedClassesId = await trx('classes').insert({
-            subject,
-            cost,
-            user_id
-        });
-
-        const class_id = insertedClassesId[0];
-
-        const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-
-            return {
-                class_id,
-                week_day: scheduleItem.week_day,
-                from: convertHourToMinutes(scheduleItem.from),
-                to: convertHourToMinutes(scheduleItem.to)
-            };
-        });
-
-        await trx('class_schedule').insert(classSchedule);
-
-        await trx.commit();
-
-        return response.status(201).send();
-    } catch (err) {
-        await trx.rollback();
-        return response.status(400).json({
-            error: 'Unexpected error while creating new class'
-        })
-    }
-});
+routes.get('/classes',  classesController.index);
 
 
 export default routes;
